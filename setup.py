a15764484e3b7855113f28e38ae7a23ecab66c26
Alistair Legione <legionea@unimelb.edu.au>
2019-09-30 20:44:29 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+LONG_DESCRIPTION = \
+'''
+This script is designed to run a series of Unix bioinformatics tools in an
+effort to assess the benefit of different alignment tools for metagenomic
+binning, in particular assessment of contig depth of coverage.
+
+'''
+
+
+setup(
+    name='MAG-sim',
+    version='0.1.0.0',
+    author='Alistair Legione',
+    author_email='legionea@unimelb.edu.au',
+    packages=['MAG-sim'],
+    package_dir={'MAG-sim': 'MAG-sim'},
+    entry_points={
+        'console_scripts': ['MAG-sim = MAG-sim.pyContigDepthAnalysisPipeline:main']
+    },
+    url='https://github.com/alegione/MAG-sim',
+    license='MIT',
+    description=('Simulation of metagenomic assemblies'),
+    long_description=(LONG_DESCRIPTION),
+    install_requires=["argparse", "os", "sys", "subprocess", "logging", "shutils",
+    "pkg_resources", "pandas", "datetime", "Bio", "numpy"],
+)
