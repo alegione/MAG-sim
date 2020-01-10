33b64e34c967bbb5a1d3032e9cda59bbf20e2320
Alistair Legione <legionea@unimelb.edu.au>
2020-01-10 17:49:22 +1100

    Update pyContigDepthAnalysisPipeline.py

--- MAGsim/pyContigDepthAnalysisPipeline.py
+++ MAGsim/pyContigDepthAnalysisPipeline.py
@@ -426,7 +426,7 @@ def main():
 
     for file in os.listdir(binning_folder):
         if os.path.isfile(binning_folder + os.path.splitext(file)[0] + ".bam") == False:
-            map_minimap2(binning_folder, binning_folder + os.path.splitext(file)[0] + ".bam", threads, binning_folder + "/" + file, Forward, Reverse)
+            map_minimap2(binning_folder + os.path.splitext(file)[0] + ".bam", threads, binning_folder + "/" + file, Forward, Reverse)
 
 #    remap_kallisto()
 
